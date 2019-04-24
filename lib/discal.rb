8a699f49daa52935c3ce1158422538c55526c745
Pablo Marti <startpablo@gmail.com>
2019-04-23 20:59:26 -0600

    Getd distance between points

--- lib/discal.rb
+++ lib/discal.rb
@@ -3,13 +3,46 @@ class Discal
 
   def initialize(coords)
     @coords = coords
+
+    raise ArgumentError, 'Argument is not an Array' unless coords.is_a? Array
+    raise ArgumentError, 'Argument must contain 2 coordinates' unless coords.count == 2
+    raise ArgumentError, 'Point 1 is not an Array' unless coords[0].is_a? Array
+    raise ArgumentError, 'Point 2 is not an Array' unless coords[1].is_a? Array
+    raise ArgumentError, 'Point 1 must be: (lat, lng)' unless coords[0].count == 2
+    raise ArgumentError, 'Point 2 must be: (lat, lng)' unless coords[1].count == 2
   end
 
   def calculate_distance_with_math
-    "x"
+    Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
   end
 
   def calculate_distance_with_api
     "y"
+  end
+
+  private
+
+  def x1
+    point1[0]
+  end
+
+  def x2
+    point2[0]
+  end
+
+  def y1
+    point1[1]
+  end
+
+  def y2
+    point2[1]
+  end
+
+  def point1
+    coords[0]
+  end
+
+  def point2
+    coords[1]
   end
 end
--- spec/discal_spec.rb
+++ spec/discal_spec.rb
@@ -1,15 +1,28 @@
 require 'discal'
 
 RSpec.describe Discal do
+  describe 'initialize' do
+    it { expect { Discal.new(nil) }.to raise_error(ArgumentError, 'Argument is not an Array') }
+    it { expect { Discal.new([1]) }.to raise_error(ArgumentError, 'Argument must contain 2 coordinates') }
+    it { expect { Discal.new([1, 2]) }.to raise_error(ArgumentError, 'Point 1 is not an Array') }
+    it { expect { Discal.new([[1], 2]) }.to raise_error(ArgumentError, 'Point 2 is not an Array') }
+    it { expect { Discal.new([[1], [3]]) }.to raise_error(ArgumentError, 'Point 1 must be: (lat, lng)') }
+    it { expect { Discal.new([[1, 2], [3]]) }.to raise_error(ArgumentError, 'Point 2 must be: (lat, lng)') }
+    it { expect { Discal.new([[1, 2], [3, 4]]) }.not_to raise_error }
+  end
+
+  coordinates = [[2.78788, 3.578787], [3.6565, 55.6434]]
+  distance = Math.sqrt((3.6565 - 2.78788) ** 2 + (55.6434 - 3.578787) ** 2)
+
   describe '#calculate_distance_with_math' do
-    d = Discal.new([[1, 2], [3, 4]])
+    d = Discal.new(coordinates)
     it 'calculates' do
-      expect(d.calculate_distance_with_math).to eq 'x'
+      expect(d.calculate_distance_with_math).to eq distance
     end
   end
 
   describe '#calculate_distance_with_api' do
-    d = Discal.new([[1, 2], [3, 4]])
+    d = Discal.new(coordinates)
     it 'calculates' do
       expect(d.calculate_distance_with_api).to eq 'y'
     end
